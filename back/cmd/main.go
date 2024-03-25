8bfeaf9e2417b0783f6211625fe370e38d0506a5
romAYAYA <supernaturalmlady@gmail.com>
2024-03-25 23:53:56 +0500

    db connected, config file made, some setup made

--- back/cmd/main.go
+++ back/cmd/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
-	todo "github.com/romAYAYA/golang-todo"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/romAYAYA/golang-todo"
 	"github.com/romAYAYA/golang-todo/pkg/handler"
+	"github.com/romAYAYA/golang-todo/pkg/repository"
+	"github.com/romAYAYA/golang-todo/pkg/service"
+	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := InitConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initilize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("An error occurred: %s", err.Error())
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
