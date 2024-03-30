b55d9bf70e77c5a5c1663964fc49d91a6b188046
romAYAYA <supernaturalmlady@gmail.com>
2024-03-30 16:36:40 +0500

    todo create & read made

--- back/pkg/service/service.go
+++ back/pkg/service/service.go
@@ -12,6 +12,9 @@ type Authorization interface {
 }
 
 type TodoList interface {
+	Create(userId int, list todo.TodoList) (int, error)
+	GetAll(userId int) ([]todo.TodoList, error)
+	GetById(userId, listId int) (todo.TodoList, error)
 }
 
 type TodoItem interface {
@@ -25,6 +28,7 @@ type Service struct {
 
 func NewService(repos *repository.Repository) *Service {
 	return &Service{
-		Authorization: newAuthService(repos.Authorization),
+		Authorization: NewAuthService(repos.Authorization),
+		TodoList:      NewTodoListService(repos.TodoList),
 	}
 }
